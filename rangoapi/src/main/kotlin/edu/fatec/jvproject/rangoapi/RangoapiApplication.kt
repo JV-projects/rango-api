172349e720d107d21bcdf63aaced3ee183ac3d2e
jackelinepaula <p.aulajackeline03@gmail.com>
2025-03-03 18:54:15 -0300

    feat: Criando o projeto

---
+++ rangoapi/src/main/kotlin/edu/fatec/jvproject/rangoapi/RangoapiApplication.kt
@@ -0,0 +1,11 @@
+package edu.fatec.jvproject.rangoapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RangoapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RangoapiApplication>(*args)
+}
---
+++ rangoapi/src/test/kotlin/edu/fatec/jvproject/rangoapi/RangoapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.fatec.jvproject.rangoapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RangoapiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
